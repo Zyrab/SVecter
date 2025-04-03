55a4ce8c4529f55cab45f4190f27dfe97ae50b69
zyrab <zurakrua@gmail.com>
2025-04-03 04:00:15 +0200

    feat: button component to modulaze and reausable

--- src/common/ToolBtn.js
+++ src/common/ToolBtn.js
@@ -1,15 +1,17 @@
 import html from "../services/DOMConstructor.js";
 
 export default function ToolBtn(tool, icon, onClick) {
+  console.log("tool", onClick);
   const spam = new html("span")
     .cls("material-symbols-outlined")
+    .css({ fontSize: "1rem" })
     .txt(icon)
     .build();
   const tooltip = new html("span").cls("tooltip").txt(tool).build();
   const button = new html("button")
     .cls("tool-btn")
     .chld([spam, tooltip])
-    .on("click", onClick)
+    .on({ click: onClick })
     .build();
   return button;
 }
