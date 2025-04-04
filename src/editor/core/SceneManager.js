ddb9b3e30c87ab80a40747827e7f68330ad82c63
zyrab <zurakrua@gmail.com>
2025-04-04 02:48:30 +0200

    added event emitor to an scene menneger when object get added we get notified

--- src/editor/core/SceneManager.js
+++ src/editor/core/SceneManager.js
@@ -1,11 +1,29 @@
 import GameObject from "../objects/GameObjects.js";
+
 class SceneManager {
   constructor() {
     this.objects = [];
+    this.eventListeners = {};
+  }
+
+  // Pub/Sub
+  on(event, callback) {
+    if (!this.eventListeners[event]) this.eventListeners[event] = [];
+    this.eventListeners[event].push(callback);
+  }
+
+  emit(event, data) {
+    (this.eventListeners[event] || []).forEach((cb) => cb(data));
   }
 
   addObject(obj) {
     this.objects.push(obj);
+    this.emit("objectListUpdated", this.getObjectList());
+  }
+
+  removeObject(obj) {
+    this.objects = this.objects.filter((o) => o !== obj);
+    this.emit("objectListUpdated", this.getObjectList());
   }
 
   draw(ctx) {
@@ -14,6 +32,10 @@ class SceneManager {
 
   getObjectAt(x, y) {
     return this.objects.find((obj) => obj.containsPoint(x, y));
+  }
+
+  getObjectList() {
+    return this.objects;
   }
 }
 
