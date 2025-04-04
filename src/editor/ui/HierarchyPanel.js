4c3d9c2fb664343dcd35fc042a42d47303b323b3
zyrab <zurakrua@gmail.com>
2025-04-04 02:49:34 +0200

    ref: hierarchi pannel now displays gameobjects from renderer scene manager

--- src/editor/ui/HierarchyPanel.js
+++ src/editor/ui/HierarchyPanel.js
@@ -1,33 +1,14 @@
-// HierarchyPanel.js
-class HierarchyPanel {
-  constructor() {
-    this.element = document.createElement("div");
-    this.element.classList.add("hierarchy-panel");
+import html from "../../services/DOMConstructor.js";
+import SectionName from "../../common/SectionName.js";
 
-    this.title = document.createElement("h2");
-    this.title.innerText = "Hierarchy";
-    this.element.appendChild(this.title);
-  }
-
-  update(objects) {
-    this.element.innerHTML = ""; // Clear the previous content
-    objects.forEach((obj) => {
-      const objDiv = document.createElement("div");
-      objDiv.innerText = obj.name;
-      objDiv.style.cursor = "pointer";
-      objDiv.addEventListener("click", () => this.handleObjectSelect(obj));
-      this.element.appendChild(objDiv);
-    });
-  }
-
-  handleObjectSelect(obj) {
-    console.log(`${obj.name} selected`);
-    // Handle object selection (maybe highlight or show in properties panel)
-  }
-
-  appendTo(container) {
-    container.appendChild(this.element);
-  }
+export default function HierarchyPanel(objs) {
+  const objects = objs.map((obj) => {
+    return new html("li").txt(obj.name).build();
+  });
+  const objectList = new html("ul").chld(objects).build();
+  const hierarchyPanel = new html("div")
+    .cls("hierarchy-panel")
+    .chld([SectionName("Hierarchy", "view_object_track"), objectList])
+    .build();
+  return hierarchyPanel;
 }
-
-export default HierarchyPanel;
