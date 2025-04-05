3bb4d846c39997f85a11d649fe1beb9128f3fc28
zyrab <zurakrua@gmail.com>
2025-04-05 05:52:55 +0200

    ref: add id to detect what icon is in use

--- src/common/MSOIcon.js
+++ src/common/MSOIcon.js
@@ -1,9 +1,10 @@
 import html from "../services/DOMConstructor.js";
 
-export default function MSOIcon(icon, fontSize = "1rem") {
+export default function MSOIcon(icon, fontSize = "1rem", id = "") {
   const spam = new html("span")
     .cls("material-symbols-outlined")
     .css({ fontSize })
+    .id(id)
     .txt(icon)
     .build();
   return spam;
