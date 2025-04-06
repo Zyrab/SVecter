5f60a7d2d87be868bf8cef3762ad6429ad20b2c1
zyrab <zurakrua@gmail.com>
2025-04-06 02:06:03 +0200

    ref: reorgnizing file system

--- src/common/SectionName.js
+++
@@ -1,9 +0,0 @@
-import html from "../services/DOMConstructor.js";
-import MSOIcon from "./MSOIcon.js";
-export default function SectionName(name, icon) {
-  const sectionName = new html("div")
-    .cls("flex align-center just-start gap-02 p-02 bg-pre-primary")
-    .chld([MSOIcon(icon), new html("h2").txt(name).build()])
-    .build();
-  return sectionName;
-}
